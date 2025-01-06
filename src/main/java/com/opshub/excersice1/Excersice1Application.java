c858a5ef1a5da4aff4f16808cafa5fdc0692be63
shruti.mishra <shruti.mishra@opshub.com>
2025-01-06 18:47:27 +0530

    setup

---
+++ src/main/java/com/opshub/excersice1/Excersice1Application.java
@@ -0,0 +1,13 @@
+package com.opshub.excersice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excersice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Excersice1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/opshub/excersice1/Excersice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opshub.excersice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Excersice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
